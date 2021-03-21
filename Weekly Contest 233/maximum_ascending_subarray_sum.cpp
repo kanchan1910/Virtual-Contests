2c15e7adc4c643342a8040efc36b1baec62f9f4e
Kanchan Jeswani <kanchanjeswani2000@gmail.com>
2021-03-21 22:06:15 +0530

    Added solutions for the weekly contest 233

---
+++ Weekly Contest 233/maximum_ascending_subarray_sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums)
+    {
+        int sum = nums[0];
+        int ans = nums[0];
+        int n = nums.size();
+        for(int i = 1 ;i < n; i++)
+        {
+            if(nums[i] > nums[i - 1])
+            {
+                sum += nums[i];
+            }
+            else
+            {
+                ans = max(ans , sum);
+                sum = nums[i];
+            }
+        }
+        ans = max(ans , sum);
+        return ans;
+    }
+};
