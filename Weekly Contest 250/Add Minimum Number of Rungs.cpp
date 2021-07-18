bb818fe26f9df40751042c7b54c782142736ad4f
Kanchan Jeswani <kanchanjeswani2000@gmail.com>
2021-07-18 17:49:00 +0530

    added solution for the problem in cpp

--- Weekly Contest 250/Add Minimum Number of Rungs.cpp
+++ Weekly Contest 250/Add Minimum Number of Rungs.cpp
@@ -24,3 +24,30 @@ public:
  
 
     
+
+
+
+class Solution {
+public:
+    int addRungs(vector<int>& rungs, int dist)
+    {
+        int ans = 0;
+        int n = rungs.size();
+        int cur = 0;
+        for(int i = 0; i < n;)
+        {
+            if(rungs[i] <= cur + dist)
+            {
+                cur = rungs[i];
+                i++;
+            }
+            else
+            {
+                ans++;
+                cur = cur + dist;
+            }
+        }
+        return ans;
+    }
+};
+// tle
